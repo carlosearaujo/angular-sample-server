6128e2046be536439e1ef35747cbd70c704fb900
Carlos <carloseaeti@gmail.com>
2017-07-03 01:57:20 -0300

    Initial Structure

---
+++ src/test/java/br/com/mv/pacientes/PacientesApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.mv.pacientes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PacientesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
